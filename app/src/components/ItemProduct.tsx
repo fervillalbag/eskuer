73a1d6713d8d40abdae1e7a3c944f653196c2b55
fervillalbag <21fervillalbag@gmail.com>
2022-04-20 23:50:06 -0400

    fix: price product value and type

--- app/src/components/ItemProduct.tsx
+++ app/src/components/ItemProduct.tsx
@@ -7,6 +7,7 @@ import { useQuery } from '@apollo/client'
 
 import 'dayjs/locale/es'
 import { GET_SUPERMARKET } from '../graphql/queries/supermarket'
+import { GET_PRODUCT } from '../graphql/queries/product'
 
 interface ItemProductIprops {
   price: any
@@ -17,12 +18,21 @@ dayjs.locale('es')
 
 const ItemProduct: React.FC<ItemProductIprops> = ({ price }) => {
   const { data: dataSupermarket } = useQuery(GET_SUPERMARKET, {
+    fetchPolicy: 'network-only',
     variables: {
       id: price?.idSuper
     }
   })
 
+  const { data: dataProduct } = useQuery(GET_PRODUCT, {
+    fetchPolicy: 'network-only',
+    variables: {
+      id: price?.idProduct
+    }
+  })
+
   const supermarket = dataSupermarket?.getSupermarket || {}
+  const product = dataProduct?.getProduct || {}
 
   return (
     <Box borderBottom="1px solid #F0F0F0" marginTop="15px" paddingBottom="15px">
@@ -47,14 +57,14 @@ const ItemProduct: React.FC<ItemProductIprops> = ({ price }) => {
               textTransform="uppercase"
               fontSize="12px"
             >
-              {dayjs(Number(price?.createdAt)).fromNow()}
+              {supermarket?.name}
             </Text>
             <Text
               // fontWeight="semibold"
               textTransform="uppercase"
               fontSize="12px"
             >
-              {supermarket?.city}
+              {dayjs(Number(price?.createdAt)).fromNow()}
             </Text>
           </Box>
         </Flex>
@@ -62,18 +72,20 @@ const ItemProduct: React.FC<ItemProductIprops> = ({ price }) => {
           <Text
             color="#3E3E3E"
             fontSize="14px"
-            textTransform="uppercase"
             textAlign="right"
             fontWeight="bold"
           >
-            {price?.value} / KG
+            Gs. {price?.value} /{' '}
+            <Text as="span" textTransform="uppercase">
+              {product.type}
+            </Text>
           </Text>
         </Box>
       </Flex>
 
       <Box marginTop="5px" gridColumn="1/5">
         <Text fontSize="12px" color="#3E3E3E" textTransform="uppercase">
-          {supermarket?.address}
+          {supermarket?.address}, {supermarket?.city}
         </Text>
       </Box>
     </Box>
